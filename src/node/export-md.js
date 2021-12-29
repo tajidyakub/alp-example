7ad55dedde41a0222f7ca484314ef6388a50bcd9
Tajid Yakub <tajid.yakub@gmail.com>
2021-12-29 19:56:35 +0700

    node markdown exporter to html

--- src/node/export-md.js
+++ src/node/export-md.js
@@ -1,24 +1,58 @@
 const fs = require('fs')
+const { basename } = require('path')
+const path = require('path')
+const { default: snarkdown } = require('snarkdown')
 const snarkdwon = require('snarkdown')
 
-const writeToFile = (content, title) => {
-    fs.writeFile('/Users/joe/test.txt', content, err => {
+const writeToFile = (folderPath,filePath,content) => {
+    let fileName = path.parse(basename(filePath)).name
+    let htmlFile = folderPath+'/html/'+fileName+'.html'
+    console.log(htmlFile)
+    fs.writeFile(htmlFile, content, err => {
         if (err) {
           console.error(err)
           return
         }
+        console.log(`File ${htmlFile} written.`)
         //file written successfully
       })
 }
- 
-try {
-  const data = fs.readFileSync('./src/notes/responsive-navbar.md', 'utf8')
-  //console.log(data)
-  const html = snarkdwon(data)
-  
-} catch (err) {
-  console.error(err)
+
+const readFile = (filePath, cb) => {
+    return fs.readFile(filePath, 'utf8' , (err, data) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        cb(data, snarkdwon)
+        return data
+      })      
 }
+
+
+const isFile = fileName => {
+    return fs.lstatSync(fileName).isFile()
+  }
+
+const folderPath = './src/notes'
+  
+const files = fs.readdirSync(folderPath).map(fileName => {
+    return path.join(folderPath, fileName)
+})
+.filter(isFile)
+
+if (files.length) {
+    files.forEach(f => {
+        readFile(f, (d,sd) => {
+            let html = sd(d)
+            writeToFile(folderPath,f,html)
+        })
+        
+    })
+}
+
+//readFile('responsive-navbar')
+
 
 
 
